500e5ad7292680bd72c934ca4441f323e726f334
muchengl <liuhanzhong.lhz@gmail.com>
2023-03-10 01:19:20 -0800

    commit

--- Makefile
+++ Makefile
@@ -5,3 +5,15 @@ server: server.c
 
 client: client.c
 	cc $(CCFLAGS) client.c -o client
+
+
+server_udp: server_udp.c
+	cc $(CCFLAGS) server_udp.c -o server_udp
+
+client_udp: client_udp.c
+	cc $(CCFLAGS) client_udp.c -o client_udp
+
+client_udp_time_test: client_udp_time_test.c
+	cc $(CCFLAGS) client_udp_time_test.c -o client_udp_time_test
+
+
--- client_udp.c
+++ client_udp.c
@@ -15,6 +15,7 @@
 #include <poll.h>
 #include <inttypes.h>
 #include <arpa/inet.h> 
+#include<signal.h>
 
 #include "stdlib.h"
 #include "time.h"
@@ -34,6 +35,18 @@ static int __chk_sys(int ret, const char *func, int line)
     return ret;
 }
 
+
+long i = 10000000L; 
+clock_t start, finish; 
+double Total_time; 
+
+double network_time=0;
+double faulting_thread_time=0;
+double handle_pagefault_time=0;
+double wait_time=0;
+
+
+
 struct faulting_thread_args { 
     uint8_t *playground; 
     int playground_size; 
@@ -43,16 +56,22 @@ static int *__faulting_thread(struct faulting_thread_args *args)
 {
     int dev_null = chk_sys(open("/dev/null", O_WRONLY)); 
 
-    for (int i=0; i < args->playground_size; i++) { 
+    
+    clock_t start=clock(); 
+
+    for (int i=0; i < args->playground_size; i+=page_size) { 
         /* Trigger a page fault by reading from playground */
         int x = args->playground[i] + 2; 
         write(dev_null, &x, sizeof(x)); 
 
         uint64_t page_boundary = ((uint64_t) &args->playground[i]) & ~(page_size - 1); 
         if ((uint64_t) &args->playground[i] == page_boundary) { 
-            printf("faulted page contents: %s\n", (char *) page_boundary); 
+            //printf("faulted page contents: %s\n", (char *) page_boundary); 
         }
     }
+
+    clock_t finish=clock(); 
+    faulting_thread_time+=(double)(finish-start) / CLOCKS_PER_SEC;
 
     chk_sys(close(dev_null)); 
     return NULL; 
@@ -64,32 +83,57 @@ static void *faulting_thread(void *args)
 }
 
 
-static void fetch_remote_page(uint8_t *page)
-{
-    int socket_fd = chk_sys(socket(AF_INET, SOCK_STREAM, 0)); 
+
+int socket_fd;
+struct sockaddr_in server_addr; 
+int page_idx=0;
+
+static void init_network(){
+    //socket_fd = chk_sys(socket(AF_INET, SOCK_STREAM, 0)); 
+    socket_fd = socket(AF_INET,SOCK_DGRAM,0);
+
     chk_sys(setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                 &(int){1}, sizeof(int)));
 
-    struct sockaddr_in server_addr; 
     memset(&server_addr, 0, sizeof(server_addr)); 
     server_addr.sin_family = AF_INET; 
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP); 
     server_addr.sin_port = htons(SERVER_PORT); 
 
-    chk_sys(connect(socket_fd, (struct sockaddr *) &server_addr,
-                sizeof(server_addr))); 
-    int bytes_read = chk_sys(read(socket_fd, page, page_size)); 
+
+}
+
+
+
+static void fetch_remote_page(uint8_t *page)
+{
+    
+    clock_t start=clock();  
+
+    // Which page is needed
+    chk_sys(sendto(socket_fd, &page_idx, sizeof(page_idx),0,(struct sockaddr*)&server_addr,sizeof(server_addr)));
+    page_idx++;
+
+    int len=sizeof(server_addr);
+
+    // Read from remote node
+    int bytes_read = chk_sys(recvfrom(socket_fd, page, page_size,0,(struct sockaddr*)&server_addr,&len)); 
     if (bytes_read != page_size) { 
-        /* Yes, yes, I know this fails if there's TCP fragmentation, etc. */
+        /* Fail, etc. */
         fprintf(stderr, "Unable to read enough bytes from server\n");
         exit(EXIT_FAILURE);
     }
 
-    chk_sys(close(socket_fd)); 
+    clock_t finish=clock(); 
+    network_time+=(double)(finish-start) / CLOCKS_PER_SEC;
+
+    //chk_sys(close(socket_fd)); 
 }
 
 static void handle_pagefault(int uffd, struct uffd_msg *uffd_msg)
 {
+   
+
     if (uffd_msg->event != UFFD_EVENT_PAGEFAULT) {
         fprintf(stderr, "Unknown event on userfaultfd\n");
         exit(EXIT_FAILURE);
@@ -100,7 +144,11 @@ static void handle_pagefault(int uffd, struct uffd_msg *uffd_msg)
 
     uint8_t *page = malloc(page_size); 
 
+
     fetch_remote_page(page); 
+
+
+    clock_t start=clock(); 
 
     struct uffdio_copy uffdio_copy; 
     memset(&uffdio_copy, 0, sizeof(uffdio_copy)); 
@@ -116,31 +164,38 @@ static void handle_pagefault(int uffd, struct uffd_msg *uffd_msg)
     }
 
     free(page); 
+
+    clock_t finish=clock(); 
+    handle_pagefault_time+=(double)(finish-start) / CLOCKS_PER_SEC;
 }
 
 static int init_userfaultfd(uint8_t *addr, int len)
 {
-    /* TODO: Do we want to handle minor page faults too? */
-    // Different operating systems vary
-    // int uffd = chk_sys(syscall(SYS_userfaultfd, UFFD_USER_MODE_ONLY));
-    int uffd = chk_sys(syscall(SYS_userfaultfd, UFFDIO_REGISTER_MODE_MISSING)); 
+
+    int uffd = chk_sys(syscall(SYS_userfaultfd, O_CLOEXEC | O_NONBLOCK)); 
 
     /* initialize userfaultfd, check for requested features */ 
     struct uffdio_api uffdio_api;
     memset(&uffdio_api, 0, sizeof(uffdio_api)); 
     uffdio_api.api = UFFD_API; 
+    //uffdio_api.features=UFFD_FEATURE_SIGBUS;
+    
     chk_sys(ioctl(uffd, UFFDIO_API, &uffdio_api)); 
 
     /* register userfaultfd handler for addr region */ 
     struct uffdio_register uffdio_register;
     memset(&uffdio_register, 0, sizeof(uffdio_register)); 
+
     uffdio_register.range.start = (unsigned long long) addr; 
     uffdio_register.range.len = len; 
     uffdio_register.mode = UFFDIO_REGISTER_MODE_MISSING; 
+
+
     chk_sys(ioctl(uffd, UFFDIO_REGISTER, &uffdio_register)); 
 
     return uffd; 
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -151,6 +206,7 @@ int main(int argc, char *argv[])
      * immediately terminate the program. Instead, we restrict userfaultfd
      * to a small memory range. 
      */
+    //int pagenum=24999;
     int pagenum=10000; 
     int playground_size = pagenum * page_size; 
 
@@ -165,6 +221,9 @@ int main(int argc, char *argv[])
 
     int uffd = init_userfaultfd(playground, playground_size); 
 
+    init_network();
+
+
     pthread_t worker; 
     struct faulting_thread_args faulting_thread_args = { 
         .playground = playground, .playground_size = playground_size 
@@ -177,41 +236,52 @@ int main(int argc, char *argv[])
     }
 
 
-    long i = 10000000L; 
+
+    //long i = 10000000L; 
     clock_t start, finish; 
     double Total_time; 
-    /* 测量一个事件持续的时间*/ 
-    printf( "Time to do %ld empty loops is ", i ); 
+    //printf( "Time to do %ld empty loops is ", i ); 
     start = clock(); 
 
+
+    
     /* Handle page faults */
-    int idx=0;
     for (;;) { 
-        idx++;
+        clock_t a1=clock();
+
         struct pollfd pollfd;
         memset(&pollfd, 0, sizeof(pollfd)); 
         pollfd.fd = uffd; 
         pollfd.events = POLLIN; 
-        chk_sys(poll(&pollfd, 1, -1)); 
+        poll(&pollfd, 1, -1); 
 
         struct uffd_msg uffd_msg;
-        if (chk_sys(read(uffd, &uffd_msg, sizeof(uffd_msg))) == 0) { 
-            fprintf(stderr, "no messages read from uffd\n"); 
+        
+        read(uffd, &uffd_msg, sizeof(uffd_msg));
 
-            exit(EXIT_FAILURE); 
-        }
+        clock_t a2=clock(); 
+        wait_time+=(double)(a2-a1) / CLOCKS_PER_SEC;
 
         handle_pagefault(uffd, &uffd_msg); 
 
-        if(idx==pagenum){
+        if(page_idx==pagenum){
             finish = clock(); 
             Total_time = (double)(finish-start) / CLOCKS_PER_SEC; 
-            printf("\nPage size: %d B\n",page_size);
-            printf( "%f seconds\n", Total_time); 
+            printf("====================\n");
+            printf("Page size: %d B  Page number: %d \n",page_size,pagenum);
+            printf("Memory: %d Mb\n",page_size*pagenum/(1000*1000));
+            printf( "%f Seconds\n", Total_time); 
+
+            printf("Network time: %f\n",network_time);
+            printf("Fault thread time: %f\n",faulting_thread_time);
+            printf("Handle pagefault time: %f\n",handle_pagefault_time);
+            printf("Wait time: %f\n",wait_time);
+            printf("Total time :%f\n",network_time+faulting_thread_time+handle_pagefault_time+wait_time);
+            printf("====================\n\n");
+            break;
         }
        
     }
-
 
     chk_sys(munmap(playground, playground_size)); 
     return EXIT_SUCCESS;
